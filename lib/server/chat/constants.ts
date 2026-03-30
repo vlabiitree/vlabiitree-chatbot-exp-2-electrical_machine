9e924362531f5f243b8036d080330683ed4c13b6
Vlab <vlabiitr.ee@gmail.com>
2026-03-30 12:17:26 +0530

    Fixed again

--- lib/server/chat/constants.ts
+++ lib/server/chat/constants.ts
@@ -1,6 +1,2 @@
 export const NO_ANSWER_MESSAGE =
   "Kindly ask questions that are related to the experiment so I can assist you better.";
-<<<<<<< HEAD
- 
-=======
->>>>>>> 1aadd9292b14834e959c376121a1a7c921091313
